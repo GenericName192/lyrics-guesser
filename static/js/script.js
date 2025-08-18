2a1fcf350d4ee9a38c103b35d8c638bfffb6a55c
GenericName192 <David-barton192@hotmail.co.uk>
2025-08-18 15:09:54 +0100

    added make_hints which splits up the lyrics

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,13 @@ async function getQuestions(){
     try {
         let questionQuery = await fetch(url);
         let questionData = await questionQuery.json();
+        return questionData
     } catch (error){
         console.log("Something has gone wrong with getting the questions")
     }
+}
+
+function make_hints(song){
+    hints = song.lyrics.split("\n")
+    return hints
 }
